8e89d7fd293059d02201c9d6d6b8c755a3b7766b
Bowen-J2 <144411446+Bowen-J2@users.noreply.github.com>
2024-07-16 22:49:17 -0700

    Add files via upload

---
+++ Priority.h
@@ -0,0 +1,14 @@
+
+#ifndef PRIORITY_H
+#define PRIORITY_H
+
+// Declaration of the enum
+enum Priority {
+    LOW = 1,
+    MEDIUM = 2,
+    HIGH = 3,
+    VERY_HIGH = 4,
+    CRITICAL = 5
+};
+
+#endif
