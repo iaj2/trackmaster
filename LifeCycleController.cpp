0f4bf9f23fe741a01fd51e5a7c6b690ca7c8b100
iaj2 <isaacjames124@yahoo.ca>
2024-07-26 14:02:37 -0700

    fix some shit

--- LifeCycleController.cpp
+++ LifeCycleController.cpp
@@ -1,10 +1,11 @@
-#include "Change.cpp";
-#include "Requester.h";
-#include "Request.h";
-#include "Product.h";
-#include "ProductRelease.h";
-#include "LifeCycleController.h";
-#include "EntityIO.h";
+#include "Change.cpp"
+#include "Requester.h"
+#include "Request.h"
+#include "Product.h"
+#include "ProductRelease.h"
+#include "LifeCycleController.h"
+#include "EntityIO.h"
+#include "globalEntities.h"
 
 EntityIO<Product> productIO("product.dat");
 EntityIO<ProductRelease> productReleaseIO("productRelease.dat");
