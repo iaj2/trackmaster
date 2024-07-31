4d53a269b254ab794e3c644d0ce6ed3609526145
adrian-sit <adriansit118@gmail.com>
2024-07-31 09:52:58 -0700

    Revert "cinignore location change"
    This reverts commit 0c64835d37ef83aebb28da246402782319a252ee.

--- UI.cpp
+++ UI.cpp
@@ -18,7 +18,6 @@ namespace UI {
 
         do {
             mainMenu();
-            cin.ignore();
             cout << "Enter Selection [0-5]: " << endl;
         
             cin >> main_choice;
@@ -27,6 +26,7 @@ namespace UI {
                 // Create Sub-Menu
                 case 1:
                     createMenu();
+                    cin.ignore();
                     cout << "Enter selection number [0-4]: ";
                     // Ignore any remaining characters in the buffer up to a newline or a large number of characters
                     cin >> create_choice;
