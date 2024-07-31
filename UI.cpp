f1a0e37cfc533aa42e7c15a07adba58fbe955adf
adrian-sit <adriansit118@gmail.com>
2024-07-31 09:58:55 -0700

    Revert "Add cin ignore in UI"
    This reverts commit 2e2e4cba02db54aded9732b68081f276b6c42c82.

--- UI.cpp
+++ UI.cpp
@@ -26,7 +26,6 @@ namespace UI {
                 // Create Sub-Menu
                 case 1:
                     createMenu();
-                    cin.ignore();
                     cout << "Enter selection number [0-4]: ";
                     // Ignore any remaining characters in the buffer up to a newline or a large number of characters
                     cin >> create_choice;
